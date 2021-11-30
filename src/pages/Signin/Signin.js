ca4b1eebfd701410804a18024f49b72161118628
tohjbin <tohjbin@gmail.com>
2021-11-30 17:42:31 +0900

    Fix - branch 이동이 안돼서 커밋함

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Footer from '../../components/Footer/Footer';
 import './Signin.scss';
+
 function Signin() {
-  return <Footer />;
+  return <div></div>;
 }
 
 export default Signin;
