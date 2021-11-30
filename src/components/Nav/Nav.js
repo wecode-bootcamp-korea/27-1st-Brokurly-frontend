b70b41ee6ee52c625c1849e4978f0212a09f0148
sodalite1204 <your.jiny.9@gmail.com>
2021-11-30 18:53:24 +0900

    Add:nav scss 일부완성 추가기능 구현중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,55 +1,73 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { HiOutlineLocationMarker } from 'react-icons/hi';
-import { BsCart2 } from 'react-icons/bs';
+import { BiSearch } from 'react-icons/bi';
+import { FiShoppingCart } from 'react-icons/fi';
+
 import './Nav.scss';
 
 function Nav() {
   return (
     <div className="nav">
-      <header className="moveHeader">
-        <div className="join">
-          <Link className="joinSignup" to="">
-            회원가입
-          </Link>
-          <Link className="joinSignin" to="">
-            로그인
-          </Link>
+      <header className="navContainer">
+        <div className="moveHeader">
+          <div className="join">
+            <Link className="joinSignup" to="">
+              회원가입
+            </Link>
+            <Link className="joinSignin" to="">
+              로그인
+            </Link>
+          </div>
+          <div className="navLogo">
+            <img src="images/dummylog.png" alt="logo" />
+          </div>
         </div>
-        <div className="navLogo">
-          <img src="images/dummylog.png" alt="logo" />
-        </div>
-      </header>
-      <div className="stickyNav">
-        <div className="categoryBar">
-          <ul className="categoryList">
-            <li className="categoryName">
-              <Link to="/vegetables">채소</Link>
-            </li>
-            <li className="categoryName">
-              <Link to="/salads">샐러드</Link>
-            </li>
-            <li className="categoryName">
-              <Link to="/fruits">과일</Link>
-            </li>
-            <li className="categoryName">
-              <Link to="/meal">간편식</Link>
-            </li>
-          </ul>
+        <div className="stickyNav">
+          <div className="categoryBar">
+            <ul className="categoryList">
+              <li className="categoryName">
+                <Link className="focusLink" to="/vegetables">
+                  채소
+                </Link>
+              </li>
+              <li className="categoryName">
+                <Link className="focusLink" to="/salads">
+                  샐러드
+                </Link>
+              </li>
+              <li className="categoryName">
+                <Link className="focusLink" to="/fruits">
+                  과일
+                </Link>
+              </li>
+              <li className="categoryName">
+                <Link className="focusLink" to="/meal">
+                  간편식
+                </Link>
+              </li>
+            </ul>
+          </div>
           <div className="navSearch">
-            <input className="searchInput" text="text" placeholder="검색" />
-            <button className="searchButton">검색</button>
+            <input
+              className="searchInput"
+              text="text"
+              placeholder="검색어를 입력해주세요.."
+            />
+            <button className="searchButton">
+              <BiSearch />
+            </button>
           </div>
           <div className="navIcon">
-            <Link to="/">
-              <HiOutlineLocationMarker />
+            <Link className="iconWrap" to="/">
+              <HiOutlineLocationMarker className="iconImage" />
             </Link>
-            <Link to="/cart">
-              <BsCart2 />
+            <Link className="iconWrap" to="/">
+              <FiShoppingCart className="iconImage" />
             </Link>
           </div>
         </div>
-      </div>
+      </header>
     </div>
   );
 }
