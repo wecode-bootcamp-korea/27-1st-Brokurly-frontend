f533bde7fe6322e662f8c629dd79d176a46fb517
sodalite1204 <your.jiny.9@gmail.com>
2021-12-01 15:30:54 +0900

    add:[상세페이지] firtst commit

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './ProductDetail.scss';
 
 function ProductDetail() {
-  return <div></div>;
+  return (
+    <div className="productDetail">
+      <article className="articleLeft">
+        <img
+          className="detailImage"
+          src="images/dummy_detail_img.jpg"
+          alt="상품"
+        />
+      </article>
+    </div>
+  );
 }
 
 export default ProductDetail;
