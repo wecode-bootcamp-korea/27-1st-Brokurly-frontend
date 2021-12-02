e8405f5d84a3ca2cc3a62b2fdfe97d60d20e9274
tohjbin <tohjbin@gmail.com>
2021-12-02 15:45:54 +0900

    브랜치 이동을 위한 커밋

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './Signup.scss';
 
 function Signup() {
-  return <div />;
+  return (
+    <div className="signup">
+      <header className="signupHeader">
+        <div className="signupHeaderName">회원가입</div>
+      </header>
+      <div className="signupBox">
+        <div className="essentialInput">
+          <span className="starMark">*필수입력사항</span>
+        </div>
+        <div className="signSection">
+          <div className="signTable"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
