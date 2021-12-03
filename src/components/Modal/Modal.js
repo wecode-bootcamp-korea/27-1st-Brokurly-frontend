67125d68847d17119ead20287579cc47affb271f
성현Maria 김 <globalkmaria@gmail.com>
2021-12-03 15:08:45 +0900

    Add: Modal 레이아웃, scss 완성

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Modal.scss';
 
-function Modal() {
-  return <div />;
+function Modal({ children, closeModal }) {
+  const modalContent = useRef();
+
+  const clickOutside = e => {
+    if (!modalContent.current.contains(e.target)) {
+      closeModal();
+    }
+  };
+
+  return (
+    <div className="modal" onClick={clickOutside}>
+      <div className="modalContent" ref={modalContent}>
+        {children}
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
