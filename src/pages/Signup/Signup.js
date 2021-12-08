c74522cac373b91248d2bbab0e26a497d48ea533
tohjbin <tohjbin@gmail.com>
2021-12-09 00:50:24 +0900

    브랜치 이동을 위한 커밋

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -58,7 +58,7 @@ function Signup() {
   };
 
   const isValidIdBtn = () => {
-    fetch(`${API.signUsername}`, {
+    fetch(API.signUsername, {
       method: 'POST',
       body: JSON.stringify({
         username: inputId,
@@ -91,7 +91,7 @@ function Signup() {
   };
 
   const isValidEmailBtn = () => {
-    fetch(`${API.signEmail}`, {
+    fetch(API.signEmail, {
       method: 'POST',
       body: JSON.stringify({
         email: inputEmail,
@@ -208,188 +208,193 @@ function Signup() {
             </div>
             <div className="signSection">
               <table className="signTable">
-                <tr className="tableTr">
-                  <th className="tableName">
-                    아이디<span className="starMark">*</span>
-                  </th>
-                  <td>
-                    <input
-                      className="tableInput"
-                      onChange={inputIdValue}
-                      onClick={openInputId}
-                      type="text"
-                      placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"
-                    />
-                    {isInputIdGuide && (
-                      <p className="guideTextBox">
-                        <span className="guideText">
-                          <div
-                            className={!isIdValid1 ? 'guideText' : 'passSign'}
-                          >
-                            <span className="dotMark">●</span> 6자 이상의 영문
-                            혹은 영문과 숫자를 조합
-                          </div>
-                        </span>
-                        <span
-                          // className={`guideText${isIdValid ? ' passSign' : ''}`
-                          className={
-                            isIdValid2 ? 'guideText passSign' : 'guideText'
-                          }
-                        >
-                          <span className="dotMark">●</span> 아이디 중복확인
-                        </span>
-                      </p>
-                    )}
-                  </td>
-                  <td>
-                    <button
-                      className="tableBtn"
-                      onClick={isValidIdBtn}
-                      type="button"
-                    >
-                      중복확인
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <th className="tableName">
-                    비밀번호<span className="starMark">*</span>
-                  </th>
-                  <td colspan="2">
-                    <input
-                      className="tableInput"
-                      onChange={inputPwValue}
-                      onClick={openInputPw}
-                      type="password"
-                      placeholder="비밀번호를 입력해주세요"
-                    />
-                    {isInputPwGuide && (
-                      <p className="guideTextBox">
-                        <span className="guideText">
-                          <div
-                            className={!isPwValid1 ? 'guideText' : 'passSign'}
-                          >
-                            <span className="dotMark">●</span> 8자 이상 입력
-                          </div>
-                        </span>
-                        <span className="guideText">
-                          <div
-                            className={!isPwValid2 ? 'guideText' : 'passSign'}
-                          >
-                            <span className="dotMark">●</span>{' '}
-                            영문/숫자/특수문자(-, 공백 제외)만 허용
-                          </div>
-                        </span>
-                        <span className="guideText">
-                          <div
-                            className={!isPwValid3 ? 'guideText' : 'passSign'}
-                          >
-                            <span className="dotMark">●</span> 동일한 숫자 및
-                            문자 3개 이상 연속 사용불가
-                          </div>
-                        </span>
-                      </p>
-                    )}
-                  </td>
-                  <td />
-                </tr>
-                <tr>
-                  <th className="tableName">
-                    비밀번호확인<span className="starMark">*</span>
-                  </th>
-                  <td colspan="2">
-                    <input
-                      className="tableInput"
-                      onChange={inputCorrectPwValue}
-                      onClick={openCorrectPw}
-                      type="password"
-                      a
-                      placeholder="비밀번호를 한번 더 입력해주세요"
-                    />
-                    {isCorrectPwGuide && (
-                      <p className="guideTextBox">
-                        <span className="guideText">
-                          <div
+                <tbody>
+                  <tr className="tableTr">
+                    <th className="tableName">
+                      아이디<span className="starMark">*</span>
+                    </th>
+                    <td>
+                      <input
+                        className="tableInput"
+                        onChange={inputIdValue}
+                        onClick={openInputId}
+                        type="text"
+                        placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"
+                      />
+                      {isInputIdGuide && (
+                        <p className="guideTextBox">
+                          <span className="guideText">
+                            <div
+                              className={!isIdValid1 ? 'guideText' : 'passSign'}
+                            >
+                              <span className="dotMark">●</span> 6자 이상의 영문
+                              혹은 영문과 숫자를 조합
+                            </div>
+                          </span>
+                          <span
+                            // className={`guideText${isIdValid ? ' passSign' : ''}`
                             className={
-                              inputCorrectPw.length !== 0 && isCorrectPwValid
-                                ? 'passSign'
-                                : 'guideText'
+                              isIdValid2 ? 'guideText passSign' : 'guideText'
                             }
                           >
-                            <span className="dotMark">●</span> 동일한 비밀번호를
-                            입력해주세요
-                          </div>
-                        </span>
-                      </p>
-                    )}
-                  </td>
-                  <td />
-                </tr>
-                <tr>
-                  <th className="tableName">
-                    이름<span className="starMark">*</span>
-                  </th>
-                  <td colspan="2">
-                    <input
-                      className="tableInput username"
-                      onChange={inputNameValue}
-                      placeholder="이름을 입력해주세요"
-                    />
-                  </td>
-                  <td />
-                </tr>
-                <tr>
-                  <th className="tableName">
-                    이메일<span className="starMark">*</span>
-                  </th>
-                  <td>
-                    <input
-                      className="tableInput email"
-                      onChange={inputEmailValue}
-                      placeholder="예: brokurly@brokurly.com"
-                    />
-                  </td>
-                  <td>
-                    <button
-                      className="tableBtn"
-                      onClick={isValidEmailBtn}
-                      type="button"
-                    >
-                      중복확인
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <th className="tableName">
-                    휴대폰<span className="starMark">*</span>
-                  </th>
-                  <td>
-                    <input
-                      className="tableInput contact"
-                      onChanage={inputContactValue}
-                      type="text"
-                      placeholder="숫자만 입력해주세요"
-                    />
-                  </td>
-                  <td>
-                    <button className="tableBtn verifiedCodeBtn" type="button">
-                      인증번호 받기
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <th className="tableName">
-                    주소<span className="starMark">*</span>
-                  </th>
-                  <td colspan="2">
-                    <input
-                      className="tableInput address"
-                      onChange={inputAddressValue}
-                      placeholder="주소를 입력해주세요"
-                    />
-                  </td>
-                  <td />
-                </tr>
+                            <span className="dotMark">●</span> 아이디 중복확인
+                          </span>
+                        </p>
+                      )}
+                    </td>
+                    <td>
+                      <button
+                        className="tableBtn"
+                        onClick={isValidIdBtn}
+                        type="button"
+                      >
+                        중복확인
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="tableName">
+                      비밀번호<span className="starMark">*</span>
+                    </th>
+                    <td colSpan="2">
+                      <input
+                        className="tableInput"
+                        onChange={inputPwValue}
+                        onClick={openInputPw}
+                        type="password"
+                        placeholder="비밀번호를 입력해주세요"
+                      />
+                      {isInputPwGuide && (
+                        <p className="guideTextBox">
+                          <span className="guideText">
+                            <div
+                              className={!isPwValid1 ? 'guideText' : 'passSign'}
+                            >
+                              <span className="dotMark">●</span> 8자 이상 입력
+                            </div>
+                          </span>
+                          <span className="guideText">
+                            <div
+                              className={!isPwValid2 ? 'guideText' : 'passSign'}
+                            >
+                              <span className="dotMark">●</span>{' '}
+                              영문/숫자/특수문자(-, 공백 제외)만 허용
+                            </div>
+                          </span>
+                          <span className="guideText">
+                            <div
+                              className={!isPwValid3 ? 'guideText' : 'passSign'}
+                            >
+                              <span className="dotMark">●</span> 동일한 숫자 및
+                              문자 3개 이상 연속 사용불가
+                            </div>
+                          </span>
+                        </p>
+                      )}
+                    </td>
+                    <td />
+                  </tr>
+                  <tr>
+                    <th className="tableName">
+                      비밀번호확인<span className="starMark">*</span>
+                    </th>
+                    <td colSpan="2">
+                      <input
+                        className="tableInput"
+                        onChange={inputCorrectPwValue}
+                        onClick={openCorrectPw}
+                        type="password"
+                        a
+                        placeholder="비밀번호를 한번 더 입력해주세요"
+                      />
+                      {isCorrectPwGuide && (
+                        <p className="guideTextBox">
+                          <span className="guideText">
+                            <div
+                              className={
+                                inputCorrectPw.length !== 0 && isCorrectPwValid
+                                  ? 'passSign'
+                                  : 'guideText'
+                              }
+                            >
+                              <span className="dotMark">●</span> 동일한
+                              비밀번호를 입력해주세요
+                            </div>
+                          </span>
+                        </p>
+                      )}
+                    </td>
+                    <td />
+                  </tr>
+                  <tr>
+                    <th className="tableName">
+                      이름<span className="starMark">*</span>
+                    </th>
+                    <td colSpan="2">
+                      <input
+                        className="tableInput username"
+                        onChange={inputNameValue}
+                        placeholder="이름을 입력해주세요"
+                      />
+                    </td>
+                    <td />
+                  </tr>
+                  <tr>
+                    <th className="tableName">
+                      이메일<span className="starMark">*</span>
+                    </th>
+                    <td>
+                      <input
+                        className="tableInput email"
+                        onChange={inputEmailValue}
+                        placeholder="예: brokurly@brokurly.com"
+                      />
+                    </td>
+                    <td>
+                      <button
+                        className="tableBtn"
+                        onClick={isValidEmailBtn}
+                        type="button"
+                      >
+                        중복확인
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="tableName">
+                      휴대폰<span className="starMark">*</span>
+                    </th>
+                    <td>
+                      <input
+                        className="tableInput contact"
+                        onChanage={inputContactValue}
+                        type="text"
+                        placeholder="숫자만 입력해주세요"
+                      />
+                    </td>
+                    <td>
+                      <button
+                        className="tableBtn verifiedCodeBtn"
+                        type="button"
+                      >
+                        인증번호 받기
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="tableName">
+                      주소<span className="starMark">*</span>
+                    </th>
+                    <td colSpan="2">
+                      <input
+                        className="tableInput address"
+                        onChange={inputAddressValue}
+                        placeholder="주소를 입력해주세요"
+                      />
+                    </td>
+                    <td />
+                  </tr>
+                </tbody>
               </table>
             </div>
             <div className="joinSection">
