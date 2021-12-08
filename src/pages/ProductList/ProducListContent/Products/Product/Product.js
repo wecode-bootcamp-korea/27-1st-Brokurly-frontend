d5e85b4ee029cbe14c16ea976e2f23001a9d5ec0
성현Maria 김 <globalkmaria@gmail.com>
2021-12-09 00:30:03 +0900

    Add : ProductList 페이지 - 상품 클릭하면 상세페이지로, 카드버튼 누르면 Modal 나오기

--- src/pages/ProductList/ProducListContent/Products/Product/Product.js
+++ src/pages/ProductList/ProducListContent/Products/Product/Product.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import './Product.scss';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import './Product.scss';
 
 function Product({ product, putInfoIntoModal }) {
-  const { name, image, price, introduction } = product;
+  const { name, image, price, introduction, id } = product;
+  const cartBtn = useRef();
+  const navigate = useNavigate();
+  const gotoProductDetail = e => {
+    if (!cartBtn.current.contains(e.target)) {
+      navigate(`product/${id}`);
+    }
+  };
 
   return (
-    <article className="product">
+    <article className="product" onClick={gotoProductDetail}>
       <div className="imgContainer">
         <img src={image} alt={name} />
-        <button className="cartBtn" onClick={() => putInfoIntoModal(product)}>
+        <button
+          className="cartBtn"
+          ref={cartBtn}
+          onClick={() => putInfoIntoModal(product)}
+        >
           <AiOutlineShoppingCart />
         </button>
       </div>
